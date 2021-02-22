f762dc3bbe8a35b7e85e0495f1419c0307c4fcdc
SahilDefault <sahil@getdefault.in>
2021-02-22 14:25:25 +0530

    Add 3rd round automation in recruiter job

--- homerun_credentials.py
+++ homerun_credentials.py
@@ -2,6 +2,6 @@ user_email = "Tejasvi@getdefault.in"
 user_pass = "Default122"
 
 emailAddress = "sahilkanger27@gmail.com"
-emailPassword = "Sahil@321"
+emailPassword = "Kanger@123"
 
 send_mail_to = "sahil@getdefault.in"
